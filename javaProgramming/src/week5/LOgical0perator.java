0722a70d5efc159645ed0c5e120ea0f98618fd8f
admin <admin@DESKTOP-RO4EHRD>
2024-04-03 11:48:19 +0900

    5주차 과제

---
+++ javaProgramming/src/week5/LOgical0perator.java
@@ -0,0 +1,24 @@
+package week5;
+
+public class LOgical0perator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println('a'>'b');
+		System.out.println(3>=2);
+		System.out.println(-1<0);
+		System.out.println(3.45<=2);
+		System.out.println(3==2);
+		System.out.println(3!=2);
+		System.out.println(!(3!=2));
+				
+		System.out.println((3>2)&&(3>4));
+		System.out.println((3!=2)||(-1>0));
+		System.out.println((3!=2)^(-1>0));
+			
+
+	}
+
+}
---
+++ javaProgramming/src/week5/successorfail.java
@@ -0,0 +1,21 @@
+package week5;
+
+import java.util.Scanner;
+
+public class successorfail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Scanner = new Scanner(System.in);
+		
+		System.out.println("점수를 입력하시오.");
+		int score =Scanner.nextInt();
+		if(score>=80)
+		 System.out.println("축하합니다! 합격입니다.");
+		
+		Scanner.close();
+		
+
+	}
+
+}
